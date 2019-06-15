d89adffa63a1c2d6f67d5424e0ecad138b4b0196
OhChanHee <och5454@naver.com>
2019-06-15 10:46:54 +0900

    6월 15일 합산본
    인트로:완성
    타이틀:설정창완성

---
+++ Assets/Scripts/Title_script/Choose_Title.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Choose_Title : MonoBehaviour
+{
+    public GameObject option;
+    public GameObject exit_Tap;
+    public GameObject load_Tap;
+    public void OnClick_NewStart()
+    {
+        GetComponent<Text>().color = Color.red;
+    }
+
+    public void OnClick_Load()
+    {
+        load_Tap.SetActive(true);
+    }
+
+    public void OnClick_Option()
+    {
+        option.SetActive(true);
+    }
+
+    public void OnClick_Exit()
+    {
+        exit_Tap.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Title_script/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+    public AudioSource BGM_Speaker;
+    public AudioSource Effect_Speaker;
+    public AudioClip[] BGM;
+    public AudioClip[] EffectSound;
+
+    public static SoundManager Get_instance()
+    {
+        if(instance == null)
+        {
+            instance = (SoundManager)FindObjectOfType(typeof(SoundManager));
+        }
+
+        return instance;
+    }
+  
+}
