abfbc5da5cd9acb6178e8e675a27d636b79c2ae8
nknk610 <48229973+nknk610@users.noreply.github.com>
2019-06-29 15:41:59 +0900

    Add BullyingPrevent Scene and SceneBase

---
+++ Assets/Scripts/UI_script/SceneBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public abstract class SceneBase : MonoBehaviour
+{
+    public Canvas canvas;
+    protected Action endScene = () => { };
+    protected float fadeInTime = 1f;
+    protected float fadeOutTime = 1f;
+    protected String nextScene = null;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        canvas.GetComponent<CanvasGroup>().alpha = 0f;
+        StartCoroutine(UIEffect.Fade(canvas.GetComponent<CanvasGroup>(), 1f ,fadeInTime));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void EndScene()
+    {
+        endScene();
+        if(nextScene != null)
+        {
+            StartCoroutine(TaskManager.Delay(fadeOutTime, () =>
+            {
+                SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+            }));
+        }
+        StartCoroutine(UIEffect.Fade(canvas.GetComponent<CanvasGroup>(), 0f, fadeOutTime));
+    }
+}
