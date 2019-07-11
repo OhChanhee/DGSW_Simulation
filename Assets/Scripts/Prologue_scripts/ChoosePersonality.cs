46af874c62b80c2f3bdcd8a519a98d35fd695682
nknk610 <48229973+nknk610@users.noreply.github.com>
2019-07-11 11:44:40 +0900

    Add Prologue_personality
    1.  프롤로그에서 성격 선택 씬 추가
    2.  캐릭터 매니저에 변수 추가

---
+++ Assets/Scripts/Prologue_scripts/ChoosePersonality.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoosePersonality : SceneBase
+{
+    public Button yes;
+    public Button no;
+    public Button report;
+    public Button stop;
+    public Text textComponent;
+
+    bool isExtroverted;
+    bool isEmotional;
+
+    string content;
+    string[] contentList;
+
+    void Awake()
+    {
+        content = textComponent.text;
+        contentList =  content.Split('|');
+        textComponent.text = "";
+    }
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        yes.onClick.AddListener(ChooseY);
+        no.onClick.AddListener(ChooseN);
+        report.onClick.AddListener(ChooseReport);
+        stop.onClick.AddListener(ChooseStop);
+        report.gameObject.SetActive(false);
+        stop.gameObject.SetActive(false);
+
+        base.Start();
+
+        StartCoroutine(TaskManager.Delay(fadeInTime, () =>
+        {
+            textComponent.text = contentList[0];
+            StartCoroutine(UIEffect.ShowEachChar(textComponent, .1f));
+        }));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeQuestion()
+    {
+        yes.gameObject.SetActive(false);
+        no.gameObject.SetActive(false);
+        report.gameObject.SetActive(true);
+        stop.gameObject.SetActive(true);
+        textComponent.text = contentList[1];
+        StartCoroutine(UIEffect.ShowEachChar(textComponent, .1f));
+    }
+
+    void Submit()
+    {
+        byte extroverted = isExtroverted ? (byte)2 : (byte)0;
+        byte emotional = isEmotional ? (byte)1 : (byte)0;
+
+        CharacterManager.Get_instance().personality = (Personality)((int)(extroverted | emotional));
+        Debug.Log(CharacterManager.Get_instance().personality);
+
+        //nextScene = "Main"; //메인화면으로 이동
+        EndScene();
+    }
+
+    void ChooseY()
+    {
+        isExtroverted = true;
+        ChangeQuestion();
+    }
+
+    void ChooseN()
+    {
+        isExtroverted = false;
+        ChangeQuestion();
+    }
+
+    void ChooseStop()
+    {
+        isEmotional = true;
+        Submit();
+    }
+
+    void ChooseReport()
+    {
+        isEmotional = false;
+        Submit();
+    }
+}
