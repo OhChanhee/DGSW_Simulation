f34f258ab833b78ea07b6b5998b905fb959bfc99
nknk610 <48229973+nknk610@users.noreply.github.com>
2019-08-24 13:31:41 +0900

    Add Getter and Setter for stat
    Getter와 Setter를 추가하고 기본적인 검사(최대치 제한)을 추가하였습니다

--- Assets/Scripts/Title_script/CharacterManager.cs
+++ Assets/Scripts/Title_script/CharacterManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System;
+using UnityEngine.SceneManagement;
 
 public class CharacterManager : ScriptableObject
 {
@@ -11,6 +12,8 @@ public class CharacterManager : ScriptableObject
         EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
     }
     */
+    const int MAX_HEALTH = 100;
+    const int MAX_STAT = 1000;
 
     public static CharacterManager instance;
 
@@ -24,31 +27,308 @@ public class CharacterManager : ScriptableObject
     public int Money;
 
     // 캐릭터의 스탯
-    public int hp=100;
-    public int sociability=50;
-    public int intelligence=50;
-    public int sensibility=50;
-    public int charm=50;
-    public int fatigue=50;
-    public int stress=0;
-    public int programming=50;
-    public int music=50;
-    public int design=50;
-    public int exercise=50;
-    public int creative=50;
-    public int leadership=50;
-    public int rewardPoint=0;
+    int _hp = 100;
+    public int hp
+    {
+        get
+        {
+            return _hp;
+        }
+        set
+        {
+            _hp = Math.Min(value, MAX_HEALTH);
+        }
+    }
+
+    int _sociability=50;
+    public int sociability
+    {
+        get
+        {
+            return _sociability;
+        }
+        set
+        {
+            _sociability = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _intelligence=50;
+    public int intelligence
+    {
+        get
+        {
+            return _intelligence;
+        }
+        set
+        {
+            _intelligence = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _sensibility=50;
+    public int sensibility
+    {
+        get
+        {
+            return _sensibility;
+        }
+        set
+        {
+            _sensibility = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _charm =50;
+    public int charm
+    {
+        get
+        {
+            return _charm;
+        }
+        set
+        {
+            _charm = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _fatigue =50;
+    public int fatigue
+    {
+        get
+        {
+            return _fatigue;
+        }
+        set
+        {
+            _fatigue = Math.Min(value, MAX_HEALTH);
+        }
+    }
+
+    int _stress =0;
+    public int stress
+    {
+        get
+        {
+            return _stress;
+        }
+        set
+        {
+            _stress = Math.Min(value, MAX_HEALTH);
+        }
+    }
+
+    int _programming =50;
+    public int programming
+    {
+        get
+        {
+            return _programming;
+        }
+        set
+        {
+            _programming = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _music =50;
+    public int music
+    {
+        get
+        {
+            return _music;
+        }
+        set
+        {
+            _music = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _design =50;
+    public int design
+    {
+        get
+        {
+            return _design;
+        }
+        set
+        {
+            _design = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _exercise =50;
+    public int exercise
+    {
+        get
+        {
+            return _exercise;
+        }
+        set
+        {
+            _exercise = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _creative =50;
+    public int creative
+    {
+        get
+        {
+            return _creative;
+        }
+        set
+        {
+            _creative = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _leadership =50;
+    public int leadership
+    {
+        get
+        {
+            return _leadership;
+        }
+        set
+        {
+            _leadership = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _rewardPoint =0;
+    public int rewardPoint
+    {
+        get
+        {
+            return _rewardPoint;
+        }
+        set
+        {
+            _rewardPoint = value;
+        }
+    }
+
 
     // 프로그래밍 스탯의 세부 스탯
-    public int web=0;
-    public int windows=0;
-    public int mobile=0;
-    public int embedded=0;
-    public int server=0;
-    public int game=0;
-    public int programmingKnowledge=0;
-    public int database=0;
-    public int data_structure=0;
+    int _web =0;
+    public int web
+    {
+        get
+        {
+            return _web;
+        }
+        set
+        {
+            _web = Math.Min(value, MAX_STAT);
+        }
+    }
+
+
+    int _windows =0;
+    public int windows
+    {
+        get
+        {
+            return _windows;
+        }
+        set
+        {
+            _windows = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _mobile =0;
+    public int mobile
+    {
+        get
+        {
+            return _mobile;
+        }
+        set
+        {
+            _mobile = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _embedded =0;
+    public int embedded
+    {
+        get
+        {
+            return _embedded;
+        }
+        set
+        {
+            _embedded = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _server =0;
+    public int server
+    {
+        get
+        {
+            return _server;
+        }
+        set
+        {
+            _server = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _game =0;
+    public int game
+    {
+        get
+        {
+            return _game;
+        }
+        set
+        {
+            _game = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _programmingKnowledge =0;
+    public int programmingKnowledge
+    {
+        get
+        {
+            return _programmingKnowledge;
+        }
+        set
+        {
+            _programmingKnowledge = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _database =0;
+    public int database
+    {
+        get
+        {
+            return _database;
+        }
+        set
+        {
+            _database = Math.Min(value, MAX_STAT);
+        }
+    }
+
+    int _dataStructure =0;
+    public int dataStructure
+    {
+        get
+        {
+            return _dataStructure;
+        }
+        set
+        {
+            _dataStructure = Math.Min(value, MAX_STAT);
+        }
+    }
+
 
     public static CharacterManager Get_instance()
     {
@@ -62,9 +342,29 @@ public class CharacterManager : ScriptableObject
 
     public class Gamedate
     {
-        public System.DateTime dateTime;
+        public System.DateTime dateTime
+        {
+            get
+            {
+                return dateTime;
+            }
+            set
+            {
+                dateTime = value;
+            }
+        }
 
-        public int week;
+        public int week
+        {
+            get
+            {
+                return week;
+            }
+            set
+            {
+                week = value > 4 || value < 1 ? value % 4 + 1 : value;
+            }
+        }
     }
 
 }
