0cfc2dcf01ab8dc99b291257e61acab6738a4eda
nknk610 <48229973+nknk610@users.noreply.github.com>
2019-09-21 16:43:50 +0900

    Change ImageName
    이미지 변경을 수월하게 하기 위해 이름을 변경했습니다.

--- Assets/Scripts/EventGame/MiniGames/Result/CheckResult.cs
+++ Assets/Scripts/EventGame/MiniGames/Result/CheckResult.cs
@@ -5,14 +5,21 @@ using UnityEngine.UI;
 
 public class CheckResult : MonoBehaviour
 {
-    public Text textComponent;
+    public RawImage title;
+    public RawImage background;
+    public Button done;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = PlayerPrefs.GetString("MinigameResult");
+        string gameResult = PlayerPrefs.GetString("MinigameResult");
 
+        title.texture = Resources.Load<Texture>("/Clear/cl_" + gameResult + "Title");
+        background.texture = Resources.Load<Texture>("/Clear/cl_" + gameResult + "Bg");
+        done.image.sprite.texture = Resources.Load<Texture2D>("/Clear/cl_" + gameResult + "Confilm");
         PlayerPrefs.DeleteKey("MinigameResult");
+
+        
     }
 
     // Update is called once per frame
