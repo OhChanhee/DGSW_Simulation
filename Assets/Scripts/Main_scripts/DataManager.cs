1a514f5ae1714217fb52fad35f13544b38255614
nknk610 <48229973+nknk610@users.noreply.github.com>
2019-10-18 14:49:59 +0900

    Update DataManager
    1. DataManager의 코드를 수정하였습니다.
    - 불필요한 함수를 없애고 코드 정리용 함수들을 추가하여 가독성을 높였습니다.
    - 변수 선언의 위치를 정리하여 함수와 변수의 선언 및 정의 부분을 명확히 하였습니다.
    2. 카테고리 선택시 발생하던 IndexOutException을 수정하여 예외가 발생하지 않도록 하였습니다.
    수정 전 : 데이터를 넣는 부분 (현재 InsertActData 함수)에서 IndexOutException이 발생하는 문제가 존재했었습니다.
    
    수정사항
    - 이제 '_'를 통해 Split한 결과를 바로 참조하여 비교하지 않습니다.
    - Split의 결과를 받아 길이를 확인 후 비교함으로써 비어있는 인덱스를 참조하지 않을 것입니다.
    
    수정할 내용 (DataManager)
    1. 변수 선언을 최소화하여 에디터상에서 반복되는 작업을 줄이고자 합니다.
    2. 값을 임의로 넣는 부분들(UpdateCategory 함수)를 알고리즘화 하여 유연하게 값의 변화를 대처하고자 합니다.

--- Assets/Scripts/Main_scripts/DataManager.cs
+++ Assets/Scripts/Main_scripts/DataManager.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 public class DataManager : MonoBehaviour
 {
-    List<Dictionary<string, object>> data;
     public Button[] Categorys = new Button[3];
     public GameObject Listitem;
+    public Sprite[] CategoryImages = new Sprite[6];
+    public GameObject contents;
+    List<Dictionary<string, object>> data;
     Week week;
     eCategory CurCategory;
-    void Start()
-    {
-        
-        data = CSVReader.Read("csvFolder/Action");
-        //Listitem = Listitem.GetComponent<Acting>();
-        for (int i = 0;i<3;i++)
-        {
-            int idx = i;
-            Categorys[i].onClick.AddListener(() =>
-            {
-                Click_Category(idx);
-            });
-        }
 
-    }
     GameObject _curWeek;
     public GameObject curWeek
     {
@@ -37,10 +25,22 @@ public class DataManager : MonoBehaviour
             week = _curWeek.GetComponent<Week>();
             UpdateCategory(week);
         }
-
     }
 
-    public Sprite[] CategoryImages = new Sprite[6];
+    void Start()
+    {
+        data = CSVReader.Read("csvFolder/Action");
+        //Listitem = Listitem.GetComponent<Acting>();
+        for (int i = 0;i<3;i++)
+        {
+            int idx = i;
+            Categorys[i].onClick.AddListener(() =>
+            {
+                Click_Category(idx);
+            });
+        }
+    }
+
     void UpdateCategory(Week week)
     {
         if (week.isWeekend == false)
@@ -65,37 +65,39 @@ public class DataManager : MonoBehaviour
 
     void Click_Category(int idx)
     {
-        
         CurCategory = Categorys[idx].GetComponent<Category>().category;
         UpdateActingList();
     }
 
-    public GameObject contents;
     void UpdateActingList()
-    {       
-         SetData();
+    {
+        InitContents();
+
+        InsertActData();
     }
 
-    void SetData()
+    void InitContents()
     {
-        for (int i=0;i<contents.transform.childCount;i++)
+        for (int i = 0; i < contents.transform.childCount; i++)
         {
             Destroy(contents.transform.GetChild(i).gameObject);
         }
-       
+    }
+
+    void InsertActData()
+    {
         for (int i = 0; i < data.Count; i++)
         {
-            if(data[i]["item_var_name"].ToString().Split(new char[] { '_' })[1] == CurCategory.ToString())
+            string[] splitedData = data[i]["item_var_name"].ToString().Split(new char[] { '_' });
+
+            if (splitedData.Length > 1 && splitedData[1] == CurCategory.ToString())
             {
                 GameObject obj = Instantiate(Listitem);
                 obj.transform.SetParent(contents.transform);
                 obj.GetComponent<Acting>().Title.text = data[i]["item_name"].ToString();
                 obj.GetComponent<Acting>().Description.text = data[i]["item_desc"].ToString();
-                obj.GetComponent<Image>().sprite = Resources.Load("Main/m_schedule/" + data[i]["item_var_name"],typeof(Sprite)) as Sprite;
+                obj.GetComponent<Image>().sprite = Resources.Load("Main/m_schedule/" + data[i]["item_var_name"], typeof(Sprite)) as Sprite;
             }
         }
-        
     }
-
-
 }
