1a6e0a294a996c4515d4fbe5a6642e421b3abd02
OhChanHee <och5454@naver.com>
2019-10-30 08:39:12 +0900

    [FIX] 시작일

--- Assets/Scripts/Prologue_scripts/ChooseDate.cs
+++ Assets/Scripts/Prologue_scripts/ChooseDate.cs
@@ -31,7 +31,7 @@ public class ChooseDate : SceneBase
                 day = Int32.Parse(obj.GetComponentInChildren<Text>().text);
                 CharacterManager.Get_instance().birthday.dateTime = new DateTime(year, month, day);
                 CharacterManager.Get_instance().birthday.week = Math.Min(day / 7 + 1, 4);
-                CharacterManager.Get_instance().curdate.dateTime = new DateTime(CharacterManager.Get_instance().birthday.dateTime.Year + 16, 5, 1);
+                CharacterManager.Get_instance().curdate.dateTime = new DateTime(CharacterManager.Get_instance().birthday.dateTime.Year + 16, 3, 1);
                 nextScene = "Prologue_personality";
                 base.EndScene();
             });
